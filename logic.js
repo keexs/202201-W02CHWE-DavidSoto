2a9b241e9b26bd476cb6b26f06459c1191a414c1
David Soto <davidsotosendarrubias@gmail.com>
2022-01-23 00:55:41 +0100

    Added function to draw the grid and change color if alive

--- logic.js
+++ logic.js
@@ -1,7 +1,24 @@
-function setGrid(rows, columns) {
-  const grid = Array(rows).fill(Array(columns).fill(0));
+const { grey } = require("color-name");
+
+const rows = 10;
+const columns = 10;
+
+function setGrid() {
+  const grid = Array(rows)
+    .fill(0)
+    .map(() => Array(columns).fill(0));
   return grid;
 }
-const newGrid = setGrid(5, 10);
-newGrid[2][3] = "something";
-console.table(newGrid);
+// eslint-disable-next-line prefer-const
+let newGrid = setGrid();
+
+function drawGrid() {
+  document.body.style.background = grey;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      if (newGrid[i][j] === 1) {
+        // cambiar recuadro de color
+      }
+    }
+  }
+}
